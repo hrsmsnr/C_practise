a00139d15c73f907988c1eda98798de5e4548174
hrsmsnr <hirose0201masanori@gmail.com>
2024-09-20 19:35:57 +0900

    add text,textbox and botton

--- text.c
+++ text.c
@@ -3,22 +3,22 @@
 // ************************************
 //必要なヘッダーファイルのインクルード
 #define STRICT
- 
+
 #include <windows.h>
 #include <stdlib.h>
 #include <string.h>
 #include <tchar.h>
- 
+
 // シンボル定義及びマクロ
 #define WINDOW_WIDTH    800
 #define WINDOW_HEIGHT   600
- 
+
 //  インスタンス（グローバル変数）
 HINSTANCE hInst;
- 
+
 //  ウィンドウプロシージャのコールバック関数
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
- 
+
 int WINAPI WinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPSTR lpCmdLine,
@@ -26,9 +26,14 @@ int WINAPI WinMain(HINSTANCE hInstance,
 {
     static TCHAR szWindowClass[] = _T("Sample03");
     static TCHAR szTitle[] = _T("ウィンドウを使ったアプリのサンプル②");
-     
-    WNDCLASSEX wcex;
- 
+
+    TCHAR szAppName[] = TEXT("TestApp");
+    WNDCLASS wc;
+    HWND hwnd;
+    MSG msg;
+
+    WNDCLASSEX wcex = {0};
+
     wcex.cbSize = sizeof(WNDCLASSEX);
     wcex.style = CS_HREDRAW | CS_VREDRAW;
     wcex.lpfnWndProc = WndProc;
@@ -41,19 +46,19 @@ int WINAPI WinMain(HINSTANCE hInstance,
     wcex.lpszMenuName = NULL;
     wcex.lpszClassName = szWindowClass;
     wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
- 
+
     if (!RegisterClassEx(&wcex))
     {
         MessageBox(NULL,
             _T("RegisterClassExの処理に失敗しました"),
             _T("Sample03"),
-            NULL);
- 
+            MB_OK);
+
         return 1;
     }
- 
+
     hInst = hInstance; // グローバル変数に値を入れる
- 
+
     // The parameters to CreateWindow explained:
     // szWindowClass                : アプリケーションの名前
     // szTitle                      : タイトルバーに現れる文字列
@@ -64,7 +69,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
     // NULL                         : メニューバー（このサンプルでは使用せず）
     // hInstance                    : WinMain関数の最初のパラメータ
     // NULL                         : WM_CREATE情報（このアプリケーションでは使用せず）
-    HWND hWnd = CreateWindow(
+    hwnd = CreateWindow(
         szWindowClass,
         szTitle,
         WS_OVERLAPPEDWINDOW,
@@ -76,24 +81,23 @@ int WINAPI WinMain(HINSTANCE hInstance,
         NULL
         );
     //  ウィンドウが生成できなかった場合
-    if (!hWnd)
+    if (!hwnd)
     {
         MessageBox(NULL,
             _T("ウィンドウ生成に失敗しました!"),
             _T("Sample03"),
-            NULL);
+            MB_OK);
         return 1;
     }
- 
+
     // ウィンドウの表示に必要なパラメータ:
-    // hWnd     : CreateWindowの戻り値
+    // hwnd     : CreateWindowの戻り値
     // nCmdShow : WinMainの引数の4番目
-    ShowWindow(hWnd,
+    ShowWindow(hwnd,
         nCmdShow);
-    UpdateWindow(hWnd);
- 
+    UpdateWindow(hwnd);
+
     // メインのメッセージループ:
-    MSG msg;
     while (GetMessage(&msg, NULL, 0, 0))
     {
         TranslateMessage(&msg);
@@ -101,32 +105,59 @@ int WINAPI WinMain(HINSTANCE hInstance,
     }
     return (int)msg.wParam;
 }
- 
+
 //  ウィンドウプロシージャ（メッセージに対するコールバック関数）
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     PAINTSTRUCT ps;
     HDC hdc;
     TCHAR greeting[] = _T("Hello, World!");
+    static HWND hEdit;  // テキストボックスのハンドル
+    static HWND hButton;  // OKボタンのハンドル
     switch (message)
     {
     case WM_PAINT:
+
+hEdit = CreateWindow(
+            TEXT("EDIT"),            // クラス名
+            TEXT(""),                // 初期テキスト
+            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_LEFT, // スタイル
+            10, 50, 200, 20,         // 位置とサイズ (x, y, width, height)
+            hwnd,                    // 親ウィンドウのハンドル
+            NULL,                    // コントロールID（メニューやコマンドで使う）
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), // インスタンスハンドル
+            NULL);                   // 拡張情報
+
+        // OKボタンを作成
+        hButton = CreateWindow(
+            TEXT("BUTTON"),          // クラス名
+            TEXT("OK"),              // ボタンのテキスト
+            WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, // スタイル
+            10, 80, 80, 30,          // 位置とサイズ (x, y, width, height)
+            hwnd,                    // 親ウィンドウのハンドル
+            (HMENU)1,                // コントロールID
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE), // インスタンスハンドル
+            NULL);                   // 拡張情報
+
         //  描画処理の開始
-        hdc = BeginPaint(hWnd, &ps);
+        hdc = BeginPaint(hwnd, &ps);
         // 文字列の出力。「Hello, World!」と出力する。
         TextOut(hdc,
-            5, 5,
+            10, 10,
             greeting, _tcslen(greeting));
         //  ペイント処理の終了
-        EndPaint(hWnd, &ps);
+        EndPaint(hwnd, &ps);
         break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
     default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
+        return DefWindowProc(hwnd, message, wParam, lParam);
         break;
     }
- 
+
+
+    
+
     return 0;
 }
